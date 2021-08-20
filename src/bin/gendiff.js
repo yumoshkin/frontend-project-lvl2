f174587bef1d74b2f60ff9ec1747b99bb7d903fc
Yury Moshkin <yumoshkin@gmail.com>
2021-08-20 17:30:17 +0300

    edit gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,8 @@ import commander from 'commander';
 commander
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
 
